b7831f75b3d9ff5e4c5ca2a568b9d81afe84f87c
krishna-432001 <krishnapriya432001@gmail.com>
2024-10-06 13:02:24 +0530

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,16 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+
+// API V1
+$routes->group('api/v1', ['namespace' => 'App\Controllers\Api\V1'], function($routes) {
+    
+});
+
+
+// API V2
+$routes->group('api/v2', ['namespace' => 'App\Controllers\Api\V2'], function($routes) {
+    // Define a route for the CategoryController index method
+    $routes->get('categories', 'CategoryController::index');    
+});
