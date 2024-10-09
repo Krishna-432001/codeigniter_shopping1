2f3b5a408717290828d7d97a0dd4511245f8c920
krishna-432001 <krishnapriya432001@gmail.com>
2024-10-09 22:37:09 +0530

    image path logic added

--- app/Database/Seeds/ProductSeeder.php
+++ app/Database/Seeds/ProductSeeder.php
@@ -10,44 +10,44 @@ class ProductSeeder extends Seeder
     {
         $data = [
             [
-                'name'          => 'Product A',
+                'name'          => 'Milk',
                 'price'         => 100.00,
                 'category_id'   => 1, // Make sure this category exists
                 'brand_id'      => 1, // Make sure this brand exists
                 'qty'           => 50,
                 'alert_stock'   => 10,
-                'image_path'    => 'path/to/image_a.jpg',
-                'description'   => 'Description for Product A',
+                'image_path'    => 'images/products/milk.webp',
+                'description'   => 'Description for Milk ',
             ],
             [
-                'name'          => 'Product B',
+                'name'          => 'Mobile',
                 'price'         => 150.50,
                 'category_id'   => 2, // Make sure this category exists
                 'brand_id'      => 2, // Make sure this brand exists
                 'qty'           => 30,
                 'alert_stock'   => 5,
-                'image_path'    => 'path/to/image_b.jpg',
-                'description'   => 'Description for Product B',
+                'image_path'    => 'images/products/mobile.jfif',
+                'description'   => 'Description for Mobile',
             ],
             [
-                'name'          => 'Product C',
+                'name'          => 'labtop ',
                 'price'         => 75.00,
                 'category_id'   => 3, // Make sure this category exists
                 'brand_id'      => 3, // Make sure this brand exists
                 'qty'           => 100,
                 'alert_stock'   => 20,
-                'image_path'    => 'path/to/image_c.jpg',
-                'description'   => 'Description for Product C',
+                'image_path'    => 'images/products/labtop.webp',
+                'description'   => 'Description for labtop ',
             ],
             [
-                'name'          => 'Product D',
+                'name'          => 'vegitable',
                 'price'         => 200.00,
                 'category_id'   => 4, // Make sure this category exists
                 'brand_id'      => 4, // Make sure this brand exists
                 'qty'           => 10,
                 'alert_stock'   => 2,
-                'image_path'    => 'path/to/image_d.jpg',
-                'description'   => 'Description for Product D',
+                'image_path'    => 'images/products/vegitable.webp',
+                'description'   => 'Description for vegitable',
             ],
         ];
 
