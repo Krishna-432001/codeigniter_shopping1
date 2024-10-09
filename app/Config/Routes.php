75d2f48e627d2a3aec3653945e6fca6fa4de2914
krishna-432001 <krishnapriya432001@gmail.com>
2024-10-10 00:39:44 +0530

    routes are filter to login to display the option user options  and group by routes are added

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -15,45 +15,56 @@ $routes->get('/frontend/contact', 'HomeController::contact');
 
 
 
-// Frontend
-$routes->group('frontend', ['namespace' => 'App\Controllers'], function($routes) {
+// Frontend->auth
+$routes->group('frontend/auth', ['namespace' => 'App\Controllers'], function($routes) {
 
-    $routes->get('login', 'AuthController::login');
+    // Before Login
+    $routes->group('', ['filter' => 'authfilter:guest'], function($routes) {
 
-    $routes->post('login', 'AuthController::authenticate');
+        $routes->get('login', 'AuthController::login', ['as' => 'home.login']);
 
-    $routes->get('register', 'AuthController::register');
+        $routes->post('authenticate', 'AuthController::authenticate', ['as' => 'home.authenticate']);
+
+        $routes->get('register', 'AuthController::register', ['as' => 'home.register']);
 
 
-    $routes->post('store', 'AuthController::store');
-
-    $routes->get('product', 'ProductController::index');
-
-    $routes->get('product_detail/(:num)', 'ProductController::detail/$1'); // Product detail route
-
-    // cartcontroller
-    $routes->get('cart', 'CartController::index');
-    $routes->post('cart/add/(:num)', 'CartController::add/$1');
-    $routes->post('cart/update/(:num)', 'CartController::update/$1');
-    $routes->get('cart/remove/(:num)', 'CartController::remove/$1');
-    $routes->get('cart/clear', 'CartController::clear');
-
+        $routes->post('store', 'AuthController::store', ['as' => 'home.store']);
+    });
 });
 
+    $routes->group('frontend/cart', ['namespace' => 'App\Controllers'], function($routes) {
+
+    // after Login
+    $routes->group('', ['filter' => 'authfilter:auth'], function($routes) {
+
+        // cartcontroller
+        $routes->get('cart', 'CartController::index', ['as' => 'home.cart']); // View the cart
+        $routes->post('add/(:num)', 'CartController::add/$1', ['as' => 'home.add/(:num)']); // Add product to cart
+        $routes->post('update/(:num)', 'CartController::update/$1', ['as' => 'home.update/(:num)']); // Update cart item quantity
+        $routes->get('remove/(:num)', 'CartController::remove/$1', ['as' => 'home.remove/(:num)']); // Remove product from cart
+        $routes->get('clear', 'CartController::clear', ['as' => 'home.clear']); // Clear the cart
+    
+    });
+});
+
+    
+$routes->group('frontend/product', ['namespace' => 'App\Controllers'], function($routes) {
+
+    $routes->get('product', 'ProductController::index', ['as' => 'home.product']);
+
+    $routes->get('product_detail/(:num)', 'ProductController::detail/$1', ['as' => 'home.product_detail/(:num)']); // Product detail route
+
+    
+});
+
+
+$routes->group('frontend/category', ['namespace' => 'App\Controllers'], function($routes) {
 //categoryController
-$routes->get('categories', 'CategoryController::index');
+$routes->get('categories', 'CategoryController::index', ['as' => 'home.categories']);
 
-$routes->post('submit-category', 'CategoryController::submitCategory');
+$routes->post('submit-category', 'CategoryController::submitCategory', ['as' => 'home.submit-category']);
 
-
-
-
-// cartcontroller
-$routes->get('cart', 'CartController::index');
-$routes->post('cart/add/(:num)', 'CartController::add/$1');
-$routes->post('cart/update/(:num)', 'CartController::update/$1');
-$routes->get('cart/remove/(:num)', 'CartController::remove/$1');
-$routes->get('cart/clear', 'CartController::clear');
+});
 
 
 
