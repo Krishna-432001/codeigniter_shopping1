536be735df2f085f75a17b4072b43e7e2a064d9b
krishna-432001 <krishnapriya432001@gmail.com>
2024-10-12 11:29:07 +0530

    Create 2024-10-12-054116_CreateHistroysTable.php

---
+++ app/Database/Migrations/2024-10-12-054116_CreateHistroysTable.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateOrderHistoryTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'order_id' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+            ],
+            'status' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'comment' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'created_at' => [
+                'type'    => 'DATETIME',
+                'null'    => true,
+            ]
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('order_id', 'orders', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('order_history');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('order_history');
+    }
+}
