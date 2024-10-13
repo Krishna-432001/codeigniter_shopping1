34a71f753c3c6b4f66a79c5999f97d3688f4e5ea
krishna-432001 <krishnapriya432001@gmail.com>
2024-10-13 10:49:11 +0530

    icon added scoial media

--- app/Views/frontend/layout/footer.php
+++ app/Views/frontend/layout/footer.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
     <style>
         /* Basic styles for body and main content */
         body {
@@ -16,23 +18,31 @@
             flex: 1; /* Allow content to grow and push footer down */
         }
         footer {
-            background-color: green;
+            background-color: black;
             color: white;
             padding: 20px 0;
             text-align: center;
         }
 
-        .social-media a {
-            text-decoration: none;
-            color: #333;
-            margin: 0 10px;
-            font-size: 1.5em;
-        }
-        .social-media a:hover {
-            color: #007BFF;
-        }
         .social-media i {
-            margin-right: 8px;
+        color: #000; /* Default icon color */
+        margin-right: 8px;
+        }
+
+        .social-media a:nth-child(1) i {
+            color: #3b5998; /* Facebook blue */
+        }
+
+        .social-media a:nth-child(2) i {
+            color: #E1306C; /* Instagram pink */
+        }
+
+        .social-media a:nth-child(3) i {
+            color: #1DA1F2; /* Twitter blue */
+        }
+
+        .social-media a:hover i {
+            opacity: 0.8; /* Hover effect for icons */
         }
     </style>
 </head>
@@ -57,16 +67,16 @@
             </li>
         </ul>
         <div class="social-media">
-        <a href="https://www.facebook.com" target="_blank">
-            <i class="fab fa-facebook"></i>Facebook
-        </a>
-        <a href="https://www.instagram.com" target="_blank">
-            <i class="fab fa-instagram"></i>Instagram
-        </a>
-        <a href="https://www.twitter.com" target="_blank">
-            <i class="fab fa-twitter"></i>Twitter
-        </a>
-    </div>
+            <a href="https://www.facebook.com" target="_blank">
+                <i class="fab fa-facebook-f"></i> Facebook 
+            </a>
+            <a href="https://www.instagram.com" target="_blank">
+                <i class="fab fa-instagram"></i> Instagram 
+            </a>
+            <a href="https://www.twitter.com" target="_blank">
+                <i class="fab fa-twitter"></i> Twitter
+            </a>
+        </div>
     </div>
 </footer>
 
